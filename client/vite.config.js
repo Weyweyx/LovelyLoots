1f76f28121d6a69c675907a0869fd6f7049833c4
Weyweyx <jhnwylyn@gmail.com>
2024-12-09 22:47:42 -0500

    added new vite-build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4040,
-    open: true // Automatically open the browser when the server starts
-  },
-  build: {
-    outDir: 'dist', // Output directory for the built app
-    sourcemap: true // Include sourcemaps for easier debugging
-  }
-});
+})
