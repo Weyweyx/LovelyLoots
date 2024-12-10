725f212f902734fa998a88cc494127d2861de164
Sam Mina <samandmissy2010@gmail.com>
2024-12-09 21:49:56 -0500

    type defs resolvers strip and token utilities mostly boilerplate

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+// Define GraphQL schema
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    role: String
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    price: Float
+    stock: Int
+    category: String
+    seller: User
+  }
+
+  type Order {
+    _id: ID
+    user: User
+    products: [Product]
+    total: Float
+    status: String
+    createdAt: String
+  }
+
+  type Query {
+    users: [User]
+    products: [Product]
+    orders: [Order]
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): User
+    addProduct(name: String!, description: String!, price: Float!, category: String!): Product
+  }
+`;
+
+module.exports = typeDefs;
