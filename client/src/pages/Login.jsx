b2b38e2f2f4c18f59f3a54d303d82ba2a0b7ebe6
Caroline Bost <cbost26@gmail.com>
2024-12-09 21:00:18 -0500

    added landing page, order submit page, navigation page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -31,7 +31,7 @@ function Login(props) {
 
   return (
     <div>
-      <Link to="/SignUp">Go to Signup!</Link>
+      <Link to="/signup">Go to Signup!</Link>
 
       <h2>Login</h2>
       <form onSubmit={handleFormSubmit}>
