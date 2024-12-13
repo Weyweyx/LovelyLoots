84df0f8c4aa8e8cb95a8ce7137b247cdf1c4db4a
Weyweyx <jhnwylyn@gmail.com>
2024-12-13 17:15:04 -0500

    styled landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,20 +4,23 @@ import Cart from "../components/Cart";
 
 const LandingPage = () => {
   return (
-    <div className="container">
-      <h1>Welcome to Lovely Loot!</h1>
+    <div className="lp">
+      <div className="lp-background"> 
+        <img src="/hero_desktop.webp" alt="" />
+      </div>
+      <div> <h1>Welcome to Lovely Loot!</h1>
         <p>At Lovely Loot, we understand the enjoyment of collecting antiques and
             finding amazing vintage items. Let us help you, with just a click of a category search, 
             you&apos;ll be shown antiques that will blow your mind! So get searching for that Lovely Loot!
         </p>
       <div className="btn-container">
         <a href="/signup">
-          <button>Sign Up!</button>
+          <button>Sign Up</button>
         </a>
         <a href="/login">
-          <button>Login!</button>
+          <button>Login</button>
         </a>
-      </div>
+      </div></div>
     </div>
   );
 };
