895b8a3f9945ddb7699bfc98a4a2668fff953ccb
Sam Mina <samandmissy2010@gmail.com>
2024-12-19 21:09:15 -0500

    fixes apollo client and proxy setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 3000,
     open: true,
     proxy: {
-      '/grapghql': {
-        target: 'http://localhost:5173',
+      '/graphql': {
+        target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true
       }
